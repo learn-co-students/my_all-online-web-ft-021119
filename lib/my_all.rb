eddb03cd4eed55c2e98d3ff9195f6dde27eca0a1
Patrick Kim <pkim050@gmail.com>
2019-02-12 08:13:43 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  arr = []
+  while i < collection.length
+    arr << yield(collection[i])
+    return false if arr[i] == false
+    i += 1
+  end
+  return true
 end
